13935843aec357dbfb24c06a8a0304605f123fa4
willymusyoka <willykyeni@gmail.com>
2025-08-07 14:12:30 +0300

    Adds instance methods to the Dog and Person classes, allowing them to perform actions like barking, sitting, talking, and walking.

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,11 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+        
+john = Person()
+john.talk()  # This will print "Hello World!"
+john.walk()  # This will print "The person is walking."    
